80a90c17a6dd43c75dacab0b1c1c982cc310f8e3
Fernando Palacios Landi <fernando.palacios@edreamsodigeo.com>
2021-10-17 15:58:06 +0200

    tailwind grid example

--- src/app/pets/components/PetList.js
+++ src/app/pets/components/PetList.js
@@ -3,7 +3,7 @@ import Pet from "./Pet";
 
 const PetList = ({ list = [] }) => {
   return (
-    <div className="search">
+    <div className="search grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {list.length === 0 ? (
         <h3>No Pets Found</h3>
       ) : (
