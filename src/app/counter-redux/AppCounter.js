64578af65d6709e72fbcd4e4934589f38ec84782
Fernando Palacios Landi <fernando.palacios@edreamsodigeo.com>
2021-11-09 12:58:10 +0100

    implement test

--- src/app/counter-redux/AppCounter.js
+++ src/app/counter-redux/AppCounter.js
@@ -1,21 +1,52 @@
-import CounterWithHooks from "./redux-hooks/components/CounterWithHooks/index.example";
-import CounterWithStorage from "./redux-storage/components/CounterWithStorage/index.example";
-// import CounterWithHooks from "./redux-hooks/components/CounterWithHooks";
-// import CounterWithStorage from "./redux-storage/components/CounterWithStorage";
+// import CounterWithHooks from "./redux-hooks/components/CounterWithHooks/index.example";
+// import CounterWithStorage from "./redux-storage/components/CounterWithStorage/index.example";
+import CounterWithHooks from "./redux-hooks/components/CounterWithHooks";
+import CounterWithStorage from "./redux-storage/components/CounterWithStorage";
 
 export default function AppCounter() {
   return (
     <div>
-      <h1>Redux implementation</h1>
+      <h1>Redux Counter implementation</h1>
       <div>
-        <h2>Use hook reducer to implement Counter</h2>
-        <CounterWithHooks />
+        <div
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            columnGap: 10,
+          }}
+        >
+          <h3>Use hook reducer</h3>
+          <CounterWithHooks />
+        </div>
+        <div>
+          <h4>implement in the files</h4>
+        </div>
+        <ul>
+          <li>CounterWithHooks/index.js</li>
+          <li>hooks/useCounterReducer.js</li>
+        </ul>
       </div>
       <br />
+      <hr />
       <br />
       <div>
-        <h2>Use Storage to implement Counter</h2>
-        <CounterWithStorage />
+        <div
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            columnGap: 10,
+          }}
+        >
+          <h3>Use storage</h3>
+          <CounterWithStorage />
+        </div>
+        <div>
+          <h4>implement in the files</h4>
+        </div>
+        <ul>
+          <li>CounterWithStorage/index.js</li>
+          <li>hooks/useCounterStorage.js</li>
+        </ul>
       </div>
     </div>
   );
